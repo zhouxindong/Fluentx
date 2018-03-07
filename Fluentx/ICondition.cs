0cf7b783e1566bd9bff0ff2ba9157062de78754b
zhouxindong <zhouxindong@163.com>
2018-03-07 08:31:40 +0800

    interface 1

---
+++ Fluentx/ICondition.cs
@@ -0,0 +1,115 @@
+﻿using System;
+
+namespace Fluentx
+{
+    /// <summary>
+    /// Any condition action.
+    /// </summary>
+    public interface IConditionalAction : IAction
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IAction Else(Action action);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder ElseIf(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder ElseIf(bool condition);
+
+    }
+
+    /// <summary>
+    /// Any condition builder.
+    /// </summary>
+    public interface IConditionBuilder : IFluent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IConditionalAction Then(Action action);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder And(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder And(bool condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder AndNot(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder AndNot(bool condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Or(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Or(bool condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder OrNot(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder OrNot(bool condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Xor(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Xor(bool condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Xnor(Func<bool> condition);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="condition"></param>
+        /// <returns></returns>
+        IConditionBuilder Xnor(bool condition);
+    }
+}
---
+++ Fluentx/IFluent.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace Fluentx
+{
+    /// <summary>
+    /// Represnt the core of a fluent interface in fluentx
+    /// </summary>
+    [EditorBrowsable(EditorBrowsableState.Never)]
+    public interface IFluent
+    {
+        /// <summary>
+        /// Redeclaration that hides the <see cref="object.GetType()"/> method from IntelliSense.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        Type GetType();
+
+        /// <summary>
+        /// Redeclaration that hides the <see cref="object.GetHashCode()"/> method from IntelliSense.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        int GetHashCode();
+
+        /// <summary>
+        /// Redeclaration that hides the <see cref="object.ToString()"/> method from IntelliSense.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        string ToString();
+
+        /// <summary>
+        /// Redeclaration that hides the <see cref="object.Equals(object)"/> method from IntelliSense.
+        /// </summary>
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        bool Equals(object obj);
+    }
+
+    /// <summary>
+    /// Represnt an Action for fluentx
+    /// </summary>
+    public interface IAction : IFluent
+    {
+    }
+}
