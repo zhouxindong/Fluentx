5ea2bfeb78a6cd7c1495057e0f0cb9a4f7f7aad3
zhouxindong <zhouxindong@163.com>
2018-03-13 23:00:24 +0800

    ISwitch Finished

---
+++ Fluentx/ISwitch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace Fluentx
+{
+    /// <summary>
+    /// Switch statement builder.
+    /// </summary>
+    public interface ISwitchBuilder : IFluent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="compare_operand"></param>
+        /// <returns></returns>
+        ISwitchCaseBuilder Case<T>(T compare_operand);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IAction Default(Action action);
+    }
+
+    /// <summary>
+    /// Switch case statement builder.
+    /// </summary>
+    public interface ISwitchCaseBuilder : IFluent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        ISwitchBuilder Execute(Action action);
+    }
+
+    /// <summary>
+    /// Switch statement for Types builder.
+    /// </summary>
+    public interface ISwitchTypeBuilder : IFluent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        ISwitchTypeCaseBuilder Case<T>();
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        IAction Default(Action action);
+    }
+
+    /// <summary>
+    /// Switch case statement for types builder.
+    /// </summary>
+    public interface ISwitchTypeCaseBuilder : IFluent
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
+        ISwitchTypeBuilder Execute(Action action);
+    }
+}
